acfe93dfd80e023f942ff199e448e6f2233680f6
JagerSPB <Jage7@yandex.ru>
2024-03-11 21:32:57 +0300

    learn Pyton 1

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-a = ('qwerty')
+import math
 
-for i in a:
-    print(i)
+# nKmDayly = 700
+# mTrack = 750
+#
+# print(f"Машина может проехать  дней {math.ceil(mTrack / nKmDayly)}")
+
+a = 20
+b = 21
+c = 23
+print(f"класс A: {(a // 2 + a % 2)} класс B: {b // 2 + a % 2} класс C: {c // 2 + c % 2} ")
+print(f"Всего необходимо {(a // 2 + a % 2) + b // 2 + a % 2 + c // 2 + c % 2}")
