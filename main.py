0b1ddda5103f30f9f57cb445ec06b841003e8d81
JagerSPB <Jage7@yandex.ru>
2024-03-12 19:45:38 +0300

    learn Pyton 1 steal lern

--- main.py
+++ main.py
@@ -5,8 +5,80 @@ import math
 #
 # print(f"Машина может проехать  дней {math.ceil(mTrack / nKmDayly)}")
 
-a = 20
-b = 21
-c = 23
-print(f"класс A: {(a // 2 + a % 2)} класс B: {b // 2 + a % 2} класс C: {c // 2 + c % 2} ")
-print(f"Всего необходимо {(a // 2 + a % 2) + b // 2 + a % 2 + c // 2 + c % 2}")
+# a = 20
+# b = 21
+# c = 23
+# print(f"класс A: {(a // 2 + a % 2)} класс B: {b // 2 + b % 2} класс C: {c // 2 + c % 2} ")
+# print(f"Всего необходимо {(a // 2 + a % 2) + (b // 2 + a % 2) + (c // 2 + c % 2)}")
+#
+
+
+# print("Enter year for check: ")
+# checkYear = int(input())
+# if (checkYear % 4 == 0 and checkYear % 100 != 0) or checkYear % 400 == 0:
+#     print(f"{checkYear} год високосный")
+# else:
+#     print(f"{checkYear} год не високосный")
+
+# print("Порядковый номер вагона: ")
+# i = int(input())
+# print("Номер вагона который в который сел Витя: ")
+# j = int(input())
+# if (i == j):
+#     print("невозможно определить длину поезда")
+# else:
+#     print(f"В поезде {i + j - 1} вагонов")
+
+
+# print("number: ")
+# i = float(input())
+# sum = 0
+# sumFloat = 0
+# i1 = i % 1
+# while i1 > 0:
+#     sumFloat += i1 % 10
+#     i1 = i1 // 10
+# while i > 0:
+#     sum += i % 10
+#     i = i // 10
+#
+# print(sumFloat)
+# n = 120
+#
+# # Введите ваше решение ниже
+#
+# x = n // 6
+# p = x
+# s = x
+# k = 4 * x
+# print(f"{p}  {s}  {k}")
+
+#
+# n = 385919
+#
+# # Введите ваше решение ниже
+# leftPart = n // 1000
+# rightPart = n % 1000
+# sumLeft = 0
+# while leftPart > 0:
+#     sumLeft += leftPart % 10
+#     leftPart = leftPart // 10
+#
+# sumRight = 0
+# while rightPart > 0:
+#     sumRight += rightPart % 10
+#     rightPart = rightPart // 10
+# if sumLeft == sumRight:
+#     print('yes')
+# else:
+#     print('no')
+
+
+# a = 3
+# b = 2
+# c = 4
+#
+# if c % a == 0 or c % b == 0:
+#     print("yes")
+# else:
+#     print("no")
